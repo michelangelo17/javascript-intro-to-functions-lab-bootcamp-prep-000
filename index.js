637104260355827426fc1c44cd5929b071ac68fe
Michelangelo Markus <michelangelo.markus@gmail.com>
2018-02-13 06:58:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase)
-  return "I can\'t hear you!"
+  if (string === string.toLowerCase())
+  return "I can't hear you!"
   else if (string === string.toUpperCase())
   return "YES INDEED!"
   else if (string === "I love you, Grandma.")
