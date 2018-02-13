86762eb2677baa9f4f3b98cb7402596802c3dc1d
Michelangelo Markus <michelangelo.markus@gmail.com>
2018-02-13 06:18:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,8 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if
+  var uppercase = 'HELLO!'
+  var lowercase = 'hello!'
+  var mixedCase = 'Hi there!'
+  if ()
 }
