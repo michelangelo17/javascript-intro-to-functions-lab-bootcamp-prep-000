16522fb3ac5b5b8938d52b6d4f30662742b83b9d
Michelangelo Markus <michelangelo.markus@gmail.com>
2018-02-13 06:31:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   var uppercase = "HELLO!"
+  var lowercase = "hello!"
   if (string.toUpperCase() === uppercase)
   return "YES INDEED!"
   else if (string === "I love you.")
   return "I love you, too."
+  else (string.toLowerCase() === lowercase)
+  return "I can't hear you!"
 }
