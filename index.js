d2b211066be8c182bc2b4c5c006dab59231e1442
Michelangelo Markus <michelangelo.markus@gmail.com>
2018-02-13 06:49:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  var uppercase = "HELLO!"
-  var lowercase = "hello!"
+  var uppercase = "HELLO"
+  var lowercase = "hello"
   var mixedCase = "Hi there!"
   if (lowercase.toLowerCase() === lowercase)
   return "I can't hear you!"
